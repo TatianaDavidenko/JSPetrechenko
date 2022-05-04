7fbb34222aa707e15d81ae00bc15ad5c99872f47
Tatiana Davydenko <tati.davydenko@gmail.com>
2022-05-04 19:45:02 +0200

    Home work 2 p24

--- main.js
+++ main.js
@@ -3,19 +3,33 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '1');
 
 const personalMovDB = {
-      count: numberOfFilms,
-      movies: {},
-      actors: {},
-      genres: [],
-      privat: false
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-const lastOfFilms = prompt('Один из последних просмотренных фильмов?', ' '),
-      ratingOfFilms = +prompt('На сколько оцените его?', '10'),
-      lastOfFilms2 = prompt('Один из последних просмотренных фильмов?', ' '),
-      ratingOfFilms2 = +prompt('На сколько оцените его?', '10');
+for (let i = 0; i < 2; i++) {
+    const lastOfFilms = prompt('Один из последних просмотренных фильмов?', ' '),
+        ratingOfFilms = +prompt('На сколько оцените его?', '10');
 
-personalMovDB.movies[lastOfFilms] = ratingOfFilms;
-personalMovDB.movies[lastOfFilms2] = ratingOfFilms2;
+    if (lastOfFilms != null && ratingOfFilms != null && lastOfFilms != '' && ratingOfFilms != '' &&
+        lastOfFilms.length < 50) {
+        personalMovDB.movies[lastOfFilms] = ratingOfFilms;
+    } else {
+        i--;
+    }
+}
+
+if (numberOfFilms <= 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms > 10 && numberOfFilms <= 30) {
+    alert('Вы обычный зритель');
+} else if (numberOfFilms > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
 console.log(personalMovDB);
